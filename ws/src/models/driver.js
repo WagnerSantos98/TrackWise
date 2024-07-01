0c024948ecdaf211fd6bb43bc4fe808826c4c150
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-07-01 17:19:38 -0300

    feat: implemetation models

--- ws/src/models/driver.js
+++ ws/src/models/driver.js
@@ -1,1 +1,13 @@
-const
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const driver = new Schema({
+    nome: String,
+    email: String,
+    senha: String,
+    telefone: String,
+    cnh: String,
+    veiculo: String,
+});
+
+module.exports = mongoose.model('Driver', driver);
