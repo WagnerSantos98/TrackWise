a061c12cea76f0a7a026a29235f8b0dfbe6b0113
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-07-05 17:15:14 -0300

    feat: implementação de model para rotas

--- ws/src/models/rota.js
+++ ws/src/models/rota.js
@@ -2,7 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const rota = new Schema({
-
+    veiculoId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Veiculo',
+        required: true
+    },
+    motoristaId: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Motorista',
+        required: true
+    },
+    origem: String,
+    destino: String,
+    distancia: Number,
+    duracao: Number,
+    pontosParada: Array,
 });
 
 module.exports = mongoose.model('Rota', rota);
