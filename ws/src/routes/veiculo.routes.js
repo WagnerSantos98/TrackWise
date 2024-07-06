ca0fe23b0de0cb275982012e38ef973bee4ef68b
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-07-06 17:48:23 -0300

    feat: implementação de rota parra rota

--- ws/src/routes/veiculo.routes.js
+++ ws/src/routes/veiculo.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = express.Router();
-const Veiculo= require('../models/veiculo')
+const Veiculo = require('../models/veiculo')
 
 router.post('/', async (req, res) => {
     try{
