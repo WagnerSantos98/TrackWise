2d5d25b9a0bfd6f62ba814e80f1373807370f85e
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-07-30 16:16:22 -0300

    feat: implementação  model cliente

---
+++ ws/src/models/cliente.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const cliente = new Schema({
+    nome: String,
+    cpf: String,
+    rg: String,
+    dataNascimento: Date,
+    email: String,
+    senha: String,
+    endereco: {
+        rua: String,
+        numero: String,
+        bairro: String,
+        cidade: String,
+        uf: String,
+        cep: String,
+        complemento: String,
+    },
+    contato: String,
+    dataCadastro:{
+        type: Date,
+        default: Date.now(),
+    }
+});
+
+module.exports = mongoose.model('Cliente', cliente);
