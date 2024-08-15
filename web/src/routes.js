beb5130fa9ae963b10b4c77bcf12d2e3b111b8a2
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-08-15 11:38:44 -0300

    feat: implementação de estilo e rotas

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,16 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './assets/css/style.css';
+
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 
-const Routes = () => {
+const App = () => {
     return(
         <>
             <Header/>
             <div className="container-fluid h-100">
-                <Sidebar/>
+                <div className="row  h-100">
+                    <Router>
+                        <Sidebar/> 
+                        <Routes>
+                            <Route path="/veiculos"/>    
+                        </Routes>  
+                    </Router>
+                </div>
             </div>
-            
         </>
     );
 };
 
-export default Routes;
+export default App;
