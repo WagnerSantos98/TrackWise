5a6027f39d2011d501f90136c5be5bcd4f5121c4
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-08-15 12:53:20 -0300

    chore: alteração de pastas para orgasnização

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-require('./database');
+require('./connection/database');
 
 const app = express();
 
