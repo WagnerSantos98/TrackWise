246a7538fe994f23035047c28133f1d8857f68b4
WagnerSantos98 <wagner.jesus98@outlook.com>
2024-08-16 14:53:11 -0300

    feat> umplementação da dashboard

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,5 +1,17 @@
+import person from '../../assets/img/person.png'
 const Header = () =>{
-    return <h1>Header</h1>;
+    return(
+        <header className="container-fluid d-flex justify-content-end">
+            <div className="d-flex align-items-center">
+                <div>
+                    <span className="d-block m-0 p-0 text-white">Track Wise</span>
+                    <small className="m-0 p-0">Admin</small>
+                </div>
+                <img src={person}/>
+                <span class="mdi mdi-chevron-down text-white"></span>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
--- web/src/components/Sidebar/index.js
+++ web/src/components/Sidebar/index.js
@@ -1,5 +1,38 @@
+import { Link } from 'react-router-dom';
+import logo from '../../assets/img/logo.png';
+
 const Sidebar = () =>{
-    return <h1>Sidebar</h1>;
+    return(
+        <sidebar className="col-2 h-100">
+           <img src={logo} className="img-fluid px-3 py-4"/>
+           <ul className="p-0 m-0">
+                <li>
+                    <Link to="/">
+                        <span className="mdi mdi-archive-marker"></span>
+                        <text>Entregas</text>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/motoristas">
+                        <span className="mdi mdi-card-account-details"></span>
+                        <text>Motoristas</text>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="veiculos">
+                        <span className="mdi mdi-car-back"></span>
+                        <text>Veículos</text>
+                    </Link>
+                </li>
+                <li>
+                    <Link to="rotas">
+                        <span className="mdi mdi-routes"></span>
+                        <text>Rotas</text>
+                    </Link>
+                </li>
+           </ul>
+        </sidebar>
+    );
 }
 
 export default Sidebar;
